09ff9d82620d3b32548cdbfa8e1605085c2f2a42
ajiyoshi <yoichi@ajiyoshi.org>
2011-09-06 13:18:11 +0900

    fix bug of gen_varint_1 for byte. bytes should be treated as "Length-Delimited" : Type 2

--- src/eproto_codec.erl
+++ src/eproto_codec.erl
@@ -173,7 +173,7 @@ gen_varint(Num) ->
 	join_bits(<<1:1, Rem:7>>, gen_varint(Num div ?GPB_BASE)).
 
 % tail recursive version. i wonder this version is easy to understand or not.
-% i think i will NEVER use large var int.
+% i think i will NEVER use very very large var int.
 -ifdef(gen_varint_1_tail_recursive).
 gen_varint_1(Num) when Num < ?GPB_BASE ->
 	<<0:1, Num:7>>;
@@ -233,7 +233,8 @@ gen_value(ID, ?TYPE_STRING, V) ->
 	join_bits(B, join_bits(BLen, V));	
 gen_value(ID, ?TYPE_BYTES, V) ->
 	B = make_id_wt(ID, ?WT_LEN),
-	join_bits(B, V);
+	BLen = gen_varint(byte_size(V)),
+	join_bits(B, join_bits(BLen, V));	
 gen_value(ID, Type, V) 
   when Type == ?TYPE_SINT32 orelse Type == ?TYPE_SINT64 ->
 	gen_zig_zag_value(ID, V).
@@ -259,7 +260,7 @@ get_varint_test() ->
 	?assertEqual(get_varint(<<150, 1, 67, 69>>), {150, <<67, 69>>}),
 	?assertEqual(get_varint(<<1, 67, 69>>), {1, <<67, 69>>}),
 	?assertEqual(get_varint(<<16#AC, 16#02>>), {300, <<>>}),
-	io:format("get_varint_test -- testing done~n").
+	ok.
 
 gen_varint_test() ->
 	?assertEqual(gen_varint(150), <<16#96, 16#01>>),
@@ -283,23 +284,45 @@ gen_varint_test() ->
 	?assertEqual(get_varint(gen_varint(16383)), {16383, <<>>}),
 	?assertEqual(get_varint(gen_varint(16384)), {16384, <<>>}),
 	?assertEqual(get_varint(gen_varint(16385)), {16385, <<>>}),
-	io:format("gen_varint_test -- testing done~n").
+	ok.
+
+gen_value_test() ->
+	Half = 0.5,
+	?assertEqual( gen_value(1, ?TYPE_DOUBLE, Half), <<1:5, ?WT_64BIT:3, Half:64/float>> ),
+	?assertEqual( gen_value(2, ?TYPE_DOUBLE, Half), <<2:5, ?WT_64BIT:3, Half:64/float>> ),
+	?assertEqual( gen_value(3, ?TYPE_FLOAT, Half), <<3:5, ?WT_32BIT:3, Half:32/float>> ),
+	?assertEqual( gen_value(4, ?TYPE_FLOAT, Half), <<4:5, ?WT_32BIT:3, Half:32/float>> ),
+	?assertEqual( gen_value(5, ?TYPE_BOOL, 0), <<5:5, ?WT_VARINT:3, 0:8>> ),
+	?assertEqual( gen_value(6, ?TYPE_BOOL, 1), <<6:5, ?WT_VARINT:3, 1:8>> ),
+	?assertEqual( gen_value(7, ?TYPE_UINT32, 150), <<7:5, ?WT_VARINT:3, 16#96:8, 16#01:8>> ),
+	Str = <<"123">>,
+	Len = byte_size(Str),
+	?assertEqual( gen_value(8, ?TYPE_STRING, Str), join_bits(<<8:5, ?WT_LEN:3, Len:8>>, Str) ),
+	?assertEqual( gen_value(9, ?TYPE_BYTES, Str), join_bits(<<9:5, ?WT_LEN:3, Len:8>>, Str) ),
+	ok.
 
 get_id_wt_test() ->
-	?assert(get_id_wt(<<8, 8>>) == {1, 0, <<8>>}),
-	?assert(get_id_wt(<<129, 1, 8>>) == {16, 1, <<8>>}),
-	io:format("get_id_wt_test -- testing done~n").
+	?assertEqual(get_id_wt(<<8, 8>>), {1, 0, <<8>>}),
+	?assertEqual(get_id_wt(<<129, 1, 8>>), {16, 1, <<8>>}),
+	ok.
+
+make_id_wt_test() ->
+	?assertEqual(get_id_wt(make_id_wt(1, ?WT_VARINT)), {1, ?WT_VARINT, <<>>}),
+	?assertEqual(get_id_wt(make_id_wt(16, ?WT_64BIT)), {16, ?WT_64BIT, <<>>}),
+	ok.
 
 get_value_test() -> 
-	{V, Rest} = get_value(?WT_VARINT, #field_desc{label = repeated, type = ?TYPE_UINT32}, 
-						  <<5, 25, 26, 150, 1, 27, 67, 69>>),
-	?assert({V, Rest} == {[25, 26, 150, 27], <<67, 69>>}),
-	{V1, Rest1} = get_value(?WT_VARINT, #field_desc{type = ?TYPE_SINT32}, <<150, 1, 67, 69>>),	
-	?assert({V1, Rest1} == {75, <<67, 69>>}),
-	io:format("get_value_test -- testing done~n").
+	{V1, Rest1} = get_value(?WT_VARINT, #field_desc{type = ?TYPE_UINT32, label = repeated}, 
+		<<5, 25, 26, 150, 1, 27, 67, 69>>),
+	?assertEqual({V1, Rest1}, {[25, 26, 150, 27], <<67, 69>>}),
+
+	{V2, Rest2} = get_value(?WT_VARINT, #field_desc{type = ?TYPE_SINT32},
+		<<150, 1, 67, 69>>),	
+	?assertEqual({V2, Rest2}, {75, <<67, 69>>}),
+	ok.
 
 get_msg_bin_test() ->	
 	{V, Rest} = get_msg_bin(?WT_LEN, #field_desc{type = ?TYPE_MESSAGE}, <<2, 1, 2, 67, 69>>),
-	?assert({V, Rest} == {<<1, 2>>, <<67, 69>>}),
-	io:format("get_msg_bin_test -- testing done~n").
+	?assertEqual({V, Rest}, {<<1, 2>>, <<67, 69>>}),
+	ok.
 	
